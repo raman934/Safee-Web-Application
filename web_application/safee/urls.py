965cd81b1d3bb8f14ab09b73a6af1525cce9a118
raman934 <ramangandhi392000@gmail.com>
2020-05-15 20:46:19 +0530

    Post Updation Features Added

--- web_application/safee/urls.py
+++ web_application/safee/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-	path('', views.home, name = 'safee'),
+	path('', PostListView.as_view(), name = 'safee'),
+	path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 	path('about/', views.about, name = 'safee-about'),
 	path('index/', views.index, name = 'safee-home'),
 	path('generic/', views.generic, name = 'safee-generic'),
