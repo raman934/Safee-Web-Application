bf3c84b8a1fdc7fe877a89fc9744c9a533ad930d
raman934 <ramangandhi392000@gmail.com>
2020-05-17 02:11:16 +0530

    Pagination Added

--- web_application/safee/views.py
+++ web_application/safee/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -8,6 +9,7 @@ from django.views.generic import (
     DeleteView
 )
 from .models import Post
+import datetime
 
 
 def home(request):
@@ -23,6 +25,18 @@ class PostListView(ListView):
     template_name = 'safee/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 1
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'safee/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 1
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -49,6 +63,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
+            post.date_posted = datetime.datetime.now()
             return True
         return False
 
