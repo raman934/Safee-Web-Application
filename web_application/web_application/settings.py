d0f5f8731eec4fe2db415178b58f0a29da00ccab
raman934 <ramangandhi392000@gmail.com>
2020-05-22 15:45:53 +0530

    Easier to install now

--- web_application/web_application/settings.py
+++ web_application/web_application/settings.py
@@ -138,3 +138,9 @@ TAILWIND_APP_NAME = 'theme'
 LOGIN_REDIRECT_URL = 'safee-home'
 LOGIN_URL = 'login'
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'ramangandhi93478@gmail.com'
+# EMAIL_HOST_PASSWORD = 'raman93478'
